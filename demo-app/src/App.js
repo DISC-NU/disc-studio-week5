b506eaf445ce8a6b649eb0bbcf39d9ed1fb44471
Lam-Richard <richardlam2024@gmail.com>
2021-10-31 20:36:36 -0500

    counter app

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [number, setNumber] = useState(0);
+  useEffect(() => {
+    if (number % 9 == 0 && number != 0) {
+      alert("This number is divisible by 9");
+    }
+  },[number])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br></br>
+      <div className="Banner">Here is a Simple React Counter!</div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div className="MSG">The current number is:</div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div className="number">{number}</div>
+      <br></br>
+      <div className="Buttons">
+        <div onClick={() => {setNumber(number + 1)}} className="plus">+</div>
+        <div onClick={() => {setNumber(number - 1)}} className="minus">-</div>
+      </div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div className="reset" onClick={() => {setNumber(0)}}>Reset</div>
     </div>
   );
 }
